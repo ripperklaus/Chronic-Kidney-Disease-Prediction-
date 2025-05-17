d92face8ae8996e9ae4c404b18526525367b6b49
Udityanshu pandey <104460339+ripperklaus@users.noreply.github.com>
2025-05-17 14:18:42 +0530

    Deploy to vercel

--- ckd-ui/vite.config.js
+++ ckd-ui/vite.config.js
@@ -4,4 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base:process.env.VITE_BASE_PATH||"/Chronic-Kidney-Disease-Prediction-/tree/main/ckd-ui"
 })
