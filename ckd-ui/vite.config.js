3449b5bb9522d3f856bb69e09eff43de062b88f7
Udityanshu pandey <104460339+ripperklaus@users.noreply.github.com>
2025-05-17 14:57:23 +0530

    Update vite.config.js

--- ckd-ui/vite.config.js
+++ ckd-ui/vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base:process.env.VITE_BASE_PATH||"/Chronic-Kidney-Disease-Prediction-"
+  base:process.env.VITE_BASE_PATH||"/Chronic-Kidney-Disease-Prediction-/ckd-ui"
 })
